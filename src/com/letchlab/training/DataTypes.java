fa4ce8df3844837f7a9c25f2258e714a9d0337cb
Rakesh Kumar Gedam <jaimaadanteshwari@Rakeshs-MacBook-Pro-2.local>
2021-09-03 08:50:15 -0500

    practice code

---
+++ src/com/letchlab/training/DataTypes.java
@@ -0,0 +1,31 @@
+package com.letchlab.training;
+
+public class DataTypes
+{
+    /**
+     * Practicing data types
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        byte byteNumber = 100;
+        System.out.println(byteNumber);
+        short shortNumber = 32767;
+        System.out.println(shortNumber);
+        int intNumber = 8787687;
+        System.out.println(intNumber);
+        long longNum = 5646498769879898799L;
+        System.out.println(longNum);
+        float floatNum = 10.231235687685687868f;
+        System.out.println(floatNum);
+        double doublenum = 10.231235687685687868d;
+        System.out.println(doublenum);
+        boolean isOk = true;
+        System.out.println(isOk);
+        char charvalue = 'a';
+        System.out.println(charvalue);
+        char asca = 77;
+        System.out.println(asca);
+
+    }
+}
---
+++ src/com/letchlab/training/SecondMain.java
@@ -0,0 +1,12 @@
+package com.letchlab.training;
+
+public class SecondMain
+{
+    public static void main(String[] args) {
+        String name = "My Name is ";
+        GetNames getNames = new GetNames();
+        getNames.getValue(name,1);
+        getNames.getValue(name,2);
+        getNames.getValue(name,3);
+    }
+}
